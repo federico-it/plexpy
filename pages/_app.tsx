7ae524a3bcb06932a155248fe409e919c765ce97
federico-it <fedecor@protonmail.com>
2023-01-30 00:45:09 +0100

    edit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
-
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' disableTransitionOnChange>
+      <Analytics />
       <Component {...pageProps} />
     </ThemeProvider>
   )
